6b395974e6ba61064800eef96e476befc471c6d0
Ruochen Wu <ruochen.wu@anu.edu.au>
2018-04-20 21:21:50 +1000

    Audios regenerated, fade-in & fade-out introduced.

--- resource/generate_wavs.py
+++ resource/generate_wavs.py
@@ -12,14 +12,20 @@ f_array[0] = 200
 sr = 44100
 dur = 1
 base = np.arange(sr * dur)
-data = np.sin(2 * np.pi * std_f / sr * base)
+filt = np.ones(sr * dur)
+fact = 10
+for i in range(len(filt) // fact):
+    filt[i] = fact * i / len(filt)
+    filt[len(filt) - 1 - i] = filt[i]
+data = np.sin(2 * np.pi * std_f / sr * base) * filt
+#data = np.sin(2 * np.pi * std_f / sr * base)
 fn = 'wavs/stdA.wav'
 wavfile.write(fn, sr, data)
 print(fn + ' created. ')
 for i in range(len(f_array)):
     for j in (-1, 1):
         f = std_f * np.power(2, j * f_array[i] / 1200)
-        data = np.sin(2 * np.pi * f / sr * base)
+        data = np.sin(2 * np.pi * f / sr * base) * filt
         fn = 'wavs/lv' + str(i) + ('_l' if j == -1 else '_h') + '.wav'
         wavfile.write(fn, sr, data)
         print(fn + ' created. ')
